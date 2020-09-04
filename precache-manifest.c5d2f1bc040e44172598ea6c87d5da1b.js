bbaca550f6c174139e9e91b914e8ecc6f0651b2a
Roman <romikkus@gmail.com>
2020-09-04 17:19:23 +0300

    Updates

---
+++ precache-manifest.c5d2f1bc040e44172598ea6c87d5da1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47874528194b8ab4d986ca682e486152",
+    "url": "./index.html"
+  },
+  {
+    "revision": "be785ccae25f5cbd459b",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "e0ce7d412840c1b03769",
+    "url": "./static/css/main.bada76a3.chunk.css"
+  },
+  {
+    "revision": "be785ccae25f5cbd459b",
+    "url": "./static/js/2.6c3fcc03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.6c3fcc03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0ce7d412840c1b03769",
+    "url": "./static/js/main.c32e6480.chunk.js"
+  },
+  {
+    "revision": "79d17474afd20bce64b6",
+    "url": "./static/js/runtime-main.12497659.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "./precache-manifest.c5d2f1bc040e44172598ea6c87d5da1b.js"
 );
 
 self.addEventListener('message', (event) => {
